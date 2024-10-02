534a0b826441706ae279f6f7b5a5ca6b6ac488e0
Peter Edmonds <peter@peteredm.com>
2024-10-02 09:59:30 -0700

    fix: templ formatting

--- lua/custom/plugins/modus.lua
+++ lua/custom/plugins/modus.lua
@@ -2,7 +2,7 @@ return {
   'miikanissi/modus-themes.nvim',
   priority = 1000,
   init = function()
-    vim.cmd.colorscheme 'modus_operandi'
+    vim.cmd.colorscheme 'modus_vivendi'
     vim.cmd.hi 'Comment gui=none'
   end,
 }
