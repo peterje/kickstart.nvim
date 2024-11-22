66e0959f01f50e19e5e434179d8aeceef6616828
Peter Edmonds <peter@peteredm.com>
2024-11-22 09:16:19 -0800

    fix: remove tsserver

--- init.lua
+++ init.lua
@@ -490,7 +490,6 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         gopls = {},
-        tsserver = {},
         html = {
           filetypes = { 'html', 'templ' },
         },
